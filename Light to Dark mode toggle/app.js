6a2b31295078bbcaeaf6998b0feedf2b6c22b4fd
rohan-p8 <rohanpatil0449@gmail.com>
2026-09-10 18:01:56 +0530

    JS file for logic

---
+++ Light to Dark mode toggle/app.js
@@ -0,0 +1,15 @@
+let mode = document.querySelector("button");
+
+
+function changeMode() {
+    mode = 0
+
+    if (mode === 0) {
+        document.body.style.backgroundColor =
+    }
+}
+
+
+mode.addEventListener("click", () => {
+
+});
