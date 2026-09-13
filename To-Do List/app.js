1a421ae75b508ef05da11c987b7be74769cb6e12
rohan-p8 <rohanpatil0449@gmail.com>
2026-09-13 19:00:17 +0530

    logic behind to do list

---
+++ To-Do List/app.js
@@ -0,0 +1,46 @@
+const textarea = document.querySelector("#input");
+const add = document.querySelector("#add");
+const taskList = document.querySelector("#task-list");
+
+
+function addTask() {
+
+    const taskText = textarea.value.trim();
+
+    if (taskText === "")
+        return;
+
+
+    const li = document.createElement("li");
+    li.classList.add("todo-item");
+
+    const span = document.createElement("span");
+    span.textContent = taskText;
+
+    const delBtn = document.createElement("button");
+    delBtn.textContent = "X";
+    delBtn.classList.add("del-btn");
+
+    delBtn.addEventListener("click", () => {
+        li.remove();
+    });
+
+
+    li.appendChild(span);
+    li.appendChild(delBtn);
+
+    taskList.appendChild(li);
+
+
+}
+
+
+add.addEventListener("click", addTask);
+
+
+textarea.addEventListener("keydown", (e) => {
+
+    if (e.key === "Enter") {
+        addTask();
+    }
+});
