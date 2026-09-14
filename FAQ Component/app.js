2d66cd3dc1c958e0353e033145f99bff29d89624
rohan-p8 <rohanpatil0449@gmail.com>
2026-09-14 23:17:30 +0530

    add logic

---
+++ FAQ Component/app.js
@@ -0,0 +1,33 @@
+const accordian = document.querySelector(".accordian");
+
+accordian.addEventListener("click", (e) => {
+
+    const header = e.target.closest("header");
+
+    if (!header)
+        return;
+
+    const item = header.parentElement;
+    const body = header.nextElementSibling;
+
+    const isActive = item.classList.contains("active");
+
+
+    const allItem = accordian.querySelectorAll(".item");
+
+    allItem.forEach((otherItem) => {
+
+        otherItem.classList.remove("active");
+
+        const otherBody = otherItem.querySelector(".body");
+        otherBody.computedStyleMap.maxHeight = null;
+    });
+
+
+    if (!isActive) {
+        item.classList.add("active");
+
+        body.style.maxHeight = body.scrollHeight + "px";
+    }
+
+});
